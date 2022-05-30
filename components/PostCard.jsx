b9d2ef766bed155d991afe7c455f41cf3f44387a
jj1107 <justinjohnson1234@msn.com>
2022-05-30 16:31:32 +0100

    still debugging

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-const PostCard = ({ post }) => {
-  return (
+const PostCard = ({ post }) => (
 	<div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
 		<div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-			<img 
+			<img
 				src={post.featuredImage.url}
 				alt={post.title}
-				className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
-			/>
+				className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg' />
 		</div>
 		<h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-purple-600 text-3xl font-semibold'>
 			<Link href={`/post/${post.slug}`}>
@@ -19,19 +17,18 @@ const PostCard = ({ post }) => {
 		</h1>
 		<div className='block lg:flex text-center items-center justify-center mb-8  w-full'>
 			<div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
-				<img 
+				<img
 					alt={post.author.name}
 					height='30px'
 					width='30px'
 					className='align-middle rounded-full'
-					src={post.author.photo.url}
-				/>
+					src={post.author.photo.url} />
 				<p className='inline align-middle text-gray-500 ml-2 text-lg'>{post.author.name}</p>
 			</div>
 			<div className='font-medium text-gray-500'>
 				<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-         			<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-        		</svg>
+					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+				</svg>
 				<span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
 			</div>
 		</div>
@@ -42,7 +39,6 @@ const PostCard = ({ post }) => {
 			</Link>
 		</div>
 	</div>
-  )
-};
+);
 
 export default PostCard
